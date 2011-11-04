81204953a8f086e4486478caf06c6471050b4d37
Gabriel Lim <gabriel@gooutnow.com>
2011-11-05 07:04:59 +0800

    'wassup'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ridemo::Application.config.secret_token = '19162a0bc7c055810479cf28c19e2ef842b630739949249d1b3b22b71e8662c9e08872148d12664a7b68adb95fdcdd35dfc32f218fcb380bb611ebf81dbe8a18'
